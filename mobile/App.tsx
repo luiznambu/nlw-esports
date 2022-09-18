22cb2f795d9adbb2525f3bdae9371e1e5810ca5c
Nambu <86805124+luiznambu@users.noreply.github.com>
2022-09-18 20:15:30 -0300

    modal web e mobile

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,4 +1,8 @@
+import { useRef, useEffect } from 'react';
 import { StatusBar } from 'react-native';
+
+import * as Notifications from 'expo-notifications';
+
 import {
   useFonts,
   Inter_400Regular,
@@ -11,14 +15,44 @@ import { Routes } from './src/routes';
 import { Background } from './src/components/Background';
 import { Loading } from './src/components/Loading';
 
-export default function App() {
+import './src/services/notificationConfigs';
+import { getPushNotificationToken } from './src/services/getPushNotificationToken';
+import { Subscription } from 'expo-modules-core';
 
+//como melhorar essa aplicacao mobile?
+//autenticacao, notificacoes por push, criar um ad pelo mobile?
+
+export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
     Inter_600SemiBold,
     Inter_700Bold,
     Inter_900Black
   });
+
+  const getNotificationListener = useRef<Subscription>();
+  const responseNotificationListener = useRef<Subscription>();
+
+  useEffect(() => {
+  getPushNotificationToken();
+  })
+
+  useEffect(() => {
+    getNotificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+      console.log(notification);
+    });
+
+    responseNotificationListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+      console.log(response)
+    })
+
+    return () => {
+      if(getNotificationListener.current && responseNotificationListener.current){
+        Notifications.removeNotificationSubscription(getNotificationListener.current);
+        Notifications.removeNotificationSubscription(responseNotificationListener.current);
+      }
+    }
+  },[])
 
   return (
     <Background>
